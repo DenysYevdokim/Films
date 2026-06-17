6089ca883d21545d1ea4b910d7f5b6cdec9a46b4
DenysYevdokim <ievdokimienko@gmail.com>
2026-06-17 13:49:49 +0200

    Start Project

---
+++ Films/FilmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmsApp.swift
+//  Films
+//
+//  Created by PRO on 17.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct FilmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
