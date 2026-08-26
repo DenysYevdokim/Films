6e3d9af6b8c642aea37a211b617d7142ee860cda
DenysYevdokim <ievdokimienko@gmail.com>
2026-08-26 21:18:40 +0200

    "feat: add save function to MoviesCache using JSONEncoder"

--- Films/Services/MoviesCache.swift
+++ Films/Services/MoviesCache.swift
@@ -12,9 +12,20 @@ class MoviesCache {
     private init() {}
     
     private let fileManager = FileManager.default
-
+    
     private lazy var fileURL: URL = {
         let paths = fileManager.urls(for: .cachesDirectory, in: .userDomainMask)
         return paths[0].appendingPathComponent("movies_cache.json")
     }()
+    
+   
+    func save(_ movies: [Movie]) {
+        do {
+            let data = try JSONEncoder().encode(movies)
+            try data.write(to: fileURL)
+        } catch {
+            print("Не удалось сохранить фильмы: \(error)")
+        }
+    }
+    
 }
